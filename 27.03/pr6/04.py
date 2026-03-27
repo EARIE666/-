c797c5d6392c56851cf79f9bd8809c922ebedd52
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-03-27 19:46:12 +0500

    Update 04.py

--- 27.03/pr6/04.py
+++ 27.03/pr6/04.py
@@ -1,1 +1,16 @@
-
+class NumberDivider:
+    def __init__(self):
+        self.divisible_list = []
+        self.not_divisible_list = []
+    
+    def add_number(self, n):
+        if n % 3 == 0:
+            self.divisible_list.append(n)
+        else:
+            self.not_divisible_list.append(n)
+    
+    def divisible(self):
+        return self.divisible_list
+    
+    def not_divisible(self):
+        return self.not_divisible_list
