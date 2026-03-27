45d9c95c8eb051a50742411d89b32d0f819f0d2f
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-03-27 19:47:12 +0500

    Update 08.py

--- 27.03/pr6/08.py
+++ 27.03/pr6/08.py
@@ -1,1 +1,17 @@
 
+class WordCaseSeparator:
+    def __init__(self):
+        self.upper_words = []
+        self.lower_words = []
+    
+    def add_word(self, word):
+        if word and word[0].isupper():
+            self.upper_words.append(word)
+        elif word and word[0].islower():
+            self.lower_words.append(word)
+    
+    def upper_case_words(self):
+        return self.upper_words
+    
+    def lower_case_words(self):
+        return self.lower_words
