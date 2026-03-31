f31431814a8238917b8dbd36de03d52d2a119b59
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-03-31 14:11:41 +0500

    Update 03.py

--- 27.03/pr8/03.py
+++ 27.03/pr8/03.py
@@ -1,1 +1,12 @@
 
+class Worker:
+    def work(self):
+        print("Working...")
+
+class Teacher(Worker):
+    def work(self):
+        print("Teaching...")
+
+class Programmer(Worker):
+    def work(self):
+        print("Coding...")
