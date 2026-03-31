0346013f07c133a16bde86c2941d9e9ab2844ce3
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-03-31 14:09:45 +0500

    Update 07.py

--- 27.03/pr7/07.py
+++ 27.03/pr7/07.py
@@ -1,1 +1,11 @@
 
+class EmailAccount:
+    def __init__(self):
+        self.__email = ""
+    
+    def set_email(self, email):
+        if '@' in email:
+            self.__email = email
+    
+    def get_email(self):
+        return self.__email
