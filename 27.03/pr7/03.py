cc115f2ed7a54e84bc45014e155971155c8aa0a5
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-03-31 14:08:51 +0500

    Update 03.py

--- 27.03/pr7/03.py
+++ 27.03/pr7/03.py
@@ -1,1 +1,10 @@
 
+class UserName:
+    def __init__(self):
+        self.__name = ""
+    
+    def set_name(self, name):
+        self.__name = name
+    
+    def get_name(self):
+        return self.__name.upper()
