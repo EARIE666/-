e547502d50d5854c6979d5f77d231b7fe09154bb
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-03-31 14:19:25 +0500

    Delete 27.03 directory

--- 27.03/pr8/04.py
+++
@@ -1,8 +0,0 @@
-
-class Printer:
-    def print_message(self, msg):
-        print(f"Message: {msg}")
-
-class UpperCasePrinter(Printer):
-    def print_message(self, msg):
-        print(msg.upper())
