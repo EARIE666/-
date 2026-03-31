614d6fe047fd271e4b0ae600be354fc826d9d272
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-03-31 14:12:52 +0500

    Update 08.py

--- 27.03/pr8/08.py
+++ 27.03/pr8/08.py
@@ -1,1 +1,12 @@
 
+class Logger:
+    def log(self, data):
+        print(f"Log: {data}")
+
+class ListLogger(Logger):
+    def log(self, data):
+        print(f"List log: {data}")
+
+class DictLogger(Logger):
+    def log(self, data):
+        print(f"Dict log: {data}")
