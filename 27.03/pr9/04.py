a98c80d5b90c585a6485186e34301375922648f1
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-03-31 14:14:16 +0500

    Update 04.py

--- 27.03/pr9/04.py
+++ 27.03/pr9/04.py
@@ -1,1 +1,15 @@
 
+from abc import ABC, abstractmethod
+
+class NumberProcessor(ABC):
+    @abstractmethod
+    def process(self, number):
+        pass
+
+class SquareProcessor(NumberProcessor):
+    def process(self, number):
+        return number ** 2
+
+class DoubleProcessor(NumberProcessor):
+    def process(self, number):
+        return number * 2
