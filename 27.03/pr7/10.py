cd6272b15fe2e2e185a8ad0b38353df92d172b4f
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-03-31 14:10:26 +0500

    Update 10.py

--- 27.03/pr7/10.py
+++ 27.03/pr7/10.py
@@ -1,1 +1,17 @@
 
+import math
+
+class Point2D:
+    def __init__(self):
+        self.__x = 0
+        self.__y = 0
+    
+    def set_coordinates(self, x, y):
+        self.__x = x
+        self.__y = y
+    
+    def get_coordinates(self):
+        return (self.__x, self.__y)
+    
+    def distance_from_origin(self):
+        return math.sqrt(self.__x**2 + self.__y**2)
