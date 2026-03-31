0102a07c83b7a2c0c9ecc64fcc2791cbeda9044e
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-03-31 14:13:03 +0500

    Update 09.py

--- 27.03/pr8/09.py
+++ 27.03/pr8/09.py
@@ -1,1 +1,12 @@
 
+class Device:
+    def action(self):
+        print("Doing generic action")
+
+class Phone(Device):
+    def action(self):
+        print("Making a call")
+
+class Laptop(Device):
+    def action(self):
+        print("Running software")
