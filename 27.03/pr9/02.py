8242cb9b4b508f7544c2034203aa71f714313ce8
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-03-31 14:13:53 +0500

    Update 02.py

--- 27.03/pr9/02.py
+++ 27.03/pr9/02.py
@@ -1,1 +1,15 @@
 
+from abc import ABC, abstractmethod
+
+class Vehicle(ABC):
+    @abstractmethod
+    def move(self):
+        pass
+
+class Car(Vehicle):
+    def move(self):
+        print("Driving")
+
+class Bicycle(Vehicle):
+    def move(self):
+        print("Pedaling")
