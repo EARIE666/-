a9347bc51353a75c1f1c66fd3e524fe44204094f
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-04-07 14:23:24 +0500

    Update 03.py

--- РКИС/07.04/pr10/03.py
+++ РКИС/07.04/pr10/03.py
@@ -1,1 +1,20 @@
 
+# ================== Задача 3 — Переопределение умножения ==================
+class Multiplier:
+    def __init__(self, value):
+        self.value = value
+    
+    def __mul__(self, other):
+        return Multiplier(self.value * other.value)
+    
+    def get_value(self):
+        return self.value
+
+# Проверка задачи 3
+print("\n=== Задача 3 ===")
+a = Multiplier(4)
+b = Multiplier(5)
+c = a * b
+print(c.get_value())
+
+
