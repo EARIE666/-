9d93fb1c68c05abb95eba9ec5e752851f516778d
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-04-07 14:18:57 +0500

    Update 06.py

--- РКИС/07.04/pr11/06.py
+++ РКИС/07.04/pr11/06.py
@@ -1,1 +1,17 @@
 
+# ================== Задача 6 — Декоратор с аргументами ==================
+def repeat(n):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            for _ in range(n):
+                func(*args, **kwargs)
+        return wrapper
+    return decorator
+
+@repeat(3)
+def greet():
+    print("Hi!")
+
+# Проверка задачи 6
+print("\n=== Задача 6 ===")
+greet()
