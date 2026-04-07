9d67ce46cbcb720e3bf4e8483c4c1a62c66113ed
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-04-07 14:18:14 +0500

    Update 04.py

--- РКИС/07.04/pr11/04.py
+++ РКИС/07.04/pr11/04.py
@@ -1,1 +1,11 @@
+# ================== Задача 4 — Перегрузка метода с default-параметрами ==================
+class Printer:
+    def print_message(self, msg, times=1):
+        for _ in range(times):
+            print(msg)
 
+# Проверка задачи 4
+print("\n=== Задача 4 ===")
+p = Printer()
+p.print_message("Hello")
+p.print_message("Hi", 3)
