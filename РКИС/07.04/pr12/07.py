cce1acd59cd68d2c2cd134aa8d34db50fd0aafb3
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-04-07 14:28:43 +0500

    Update 07.py

--- РКИС/07.04/pr12/07.py
+++ РКИС/07.04/pr12/07.py
@@ -1,1 +1,12 @@
 
+# Задача 7 — Индекс вне диапазона
+def get_element(lst, index):
+    try:
+        return lst[index]
+    except IndexError:
+        return "Index error"
+
+print(get_element([1, 2, 3], 1))
+print(get_element([1, 2, 3], 5))
+
+
