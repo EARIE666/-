80214ea2bce8d9d2d4959dd2958b2ce8c20d21b4
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-04-07 14:19:57 +0500

    Update 09.py

--- РКИС/07.04/pr11/09.py
+++ РКИС/07.04/pr11/09.py
@@ -1,1 +1,21 @@
 
+# ================== Задача 9 — Переопределение + декоратор ==================
+def excited(func):
+    def wrapper(*args, **kwargs):
+        print("Let's go!")
+        return func(*args, **kwargs)
+    return wrapper
+
+class Worker:
+    def work(self):
+        print("Working...")
+
+class Teacher(Worker):
+    def work(self):
+        print("Teaching...")
+
+# Проверка задачи 9
+print("\n=== Задача 9 ===")
+t = Teacher()
+t.work = excited(t.work)
+t.work()
