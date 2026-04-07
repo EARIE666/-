33b68efc366a4c23911f1cb8b97b2d2392b17d4d
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-04-07 14:24:58 +0500

    Update 08.py

--- РКИС/07.04/pr10/08.py
+++ РКИС/07.04/pr10/08.py
@@ -1,1 +1,20 @@
 
+# ================== Задача 8 — Переопределение отрицания ==================
+class BooleanWrapper:
+    def __init__(self, value):
+        self.value = value
+    
+    def __bool__(self):
+        return self.value
+    
+    def __invert__(self):
+        return BooleanWrapper(not self.value)
+
+# Проверка задачи 8 (используем not)
+print("\n=== Задача 8 ===")
+b = BooleanWrapper(True)
+print(not b)
+c = BooleanWrapper(False)
+print(not c)
+
+
