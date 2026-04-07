12dd13186345eb4f12c9b8616ec0222d8580eb4d
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-04-07 14:23:08 +0500

    Update 02.py

--- РКИС/07.04/pr10/02.py
+++ РКИС/07.04/pr10/02.py
@@ -1,1 +1,20 @@
 
+# ================== Задача 2 — Переопределение вычитания ==================
+class NumberWrapperSub:
+    def __init__(self, value):
+        self.value = value
+    
+    def __sub__(self, other):
+        return NumberWrapperSub(self.value - other.value)
+    
+    def get_value(self):
+        return self.value
+
+# Проверка задачи 2
+print("\n=== Задача 2 ===")
+a = NumberWrapperSub(10)
+b = NumberWrapperSub(3)
+c = a - b
+print(c.get_value())
+
+
