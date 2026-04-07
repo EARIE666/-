4950b5c9f5564b58100ace2c39d81afb018eb202
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-04-07 14:19:13 +0500

    Update 07.py

--- РКИС/07.04/pr11/07.py
+++ РКИС/07.04/pr11/07.py
@@ -1,1 +1,13 @@
 
+# ================== Задача 7 — Комбинированная перегрузка ==================
+class Adder:
+    def add(self, a, b=None):
+        if b is None:
+            return a + 10
+        return a + b
+
+# Проверка задачи 7
+print("\n=== Задача 7 ===")
+a = Adder()
+print(a.add(5))
+print(a.add(3, 4))
