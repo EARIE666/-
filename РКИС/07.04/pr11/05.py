fc864c75fea55338d53ed2d6e4786a8abf08d74b
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-04-07 14:18:39 +0500

    Update 05.py

--- РКИС/07.04/pr11/05.py
+++ РКИС/07.04/pr11/05.py
@@ -1,1 +1,15 @@
 
+# ================== Задача 5 — Декоратор для логирования ==================
+def logger(func):
+    def wrapper(*args, **kwargs):
+        print(f"Calling {func.__name__}")
+        return func(*args, **kwargs)
+    return wrapper
+
+@logger
+def say_hello():
+    print("Hello!")
+
+# Проверка задачи 5
+print("\n=== Задача 5 ===")
+say_hello()
