5b01b570434a81320d31c12253bb82f57795a366
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-04-07 14:27:54 +0500

    Update 04.py

--- РКИС/07.04/pr12/04.py
+++ РКИС/07.04/pr12/04.py
@@ -1,1 +1,15 @@
 
+# Задача 4 — finally
+def read_number(s):
+    try:
+        result = int(s)
+        return result
+    except ValueError:
+        return "Error"
+    finally:
+        print("Done")
+
+print(read_number("10"))
+print(read_number("abc"))
+
+
