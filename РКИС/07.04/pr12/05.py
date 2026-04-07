f376493a9dc41c51159df81f5bf30257e7955a87
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-04-07 14:28:09 +0500

    Update 05.py

--- РКИС/07.04/pr12/05.py
+++ РКИС/07.04/pr12/05.py
@@ -1,1 +1,14 @@
 
+# Задача 5 — Пользовательское исключение
+def check_age(age):
+    try:
+        if age < 0:
+            raise ValueError
+        return "OK"
+    except ValueError:
+        return "Invalid age"
+
+print(check_age(20))
+print(check_age(-5))
+
+
