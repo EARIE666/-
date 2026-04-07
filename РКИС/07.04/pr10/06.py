a44e2dde6023b3f3d4931514facec941e8fce5ed
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-04-07 14:24:25 +0500

    Update 06.py

--- РКИС/07.04/pr10/06.py
+++ РКИС/07.04/pr10/06.py
@@ -1,1 +1,21 @@
 
+# ================== Задача 6 — Переопределение равенства ==================
+class EqualNumber:
+    def __init__(self, value):
+        self.value = value
+    
+    def __eq__(self, other):
+        return self.value == other.value
+    
+    def get_value(self):
+        return self.value
+
+# Проверка задачи 6
+print("\n=== Задача 6 ===")
+a = EqualNumber(5)
+b = EqualNumber(5)
+c = EqualNumber(3)
+print(a == b)
+print(a == c)
+
+
