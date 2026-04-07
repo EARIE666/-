59ea568d335240d510fb6d80c502f9cf5c1b29c8
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-04-07 14:27:06 +0500

    Update 01.py

--- РКИС/07.04/pr12/01.py
+++ РКИС/07.04/pr12/01.py
@@ -1,1 +1,14 @@
 
+# Практическая работа №12 – Обработка исключений
+
+# Задача 1 — Перехват ошибки деления
+def safe_divide(a, b):
+    try:
+        return a / b
+    except ZeroDivisionError:
+        return "Division by zero"
+
+print(safe_divide(10, 2))
+print(safe_divide(5, 0))
+
+
