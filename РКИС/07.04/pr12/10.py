7f3c924775d5a67381ad6fc866850d4ed1aa8aa4
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-04-07 14:29:28 +0500

    Update 10.py

--- РКИС/07.04/pr12/10.py
+++ РКИС/07.04/pr12/10.py
@@ -1,1 +1,24 @@
 
+# Задача 10 — Комплексная обработка
+def calculator(a, b, op):
+    try:
+        if op == "+":
+            return a + b
+        elif op == "-":
+            return a - b
+        elif op == "*":
+            return a * b
+        elif op == "/":
+            try:
+                return a / b
+            except ZeroDivisionError:
+                return "Division by zero"
+        else:
+            return "Unknown operation"
+    except TypeError:
+        return "Type error"
+
+print(calculator(5, 3, "+"))
+print(calculator(5, 0, "/"))
+print(calculator(5, 3, "^"))
+print(calculator(5, "a", "+"))
