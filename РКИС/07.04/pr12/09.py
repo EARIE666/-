727bce4a30a01859c1fd4c0d943c6da2e2c25398
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-04-07 14:29:14 +0500

    Update 09.py

--- РКИС/07.04/pr12/09.py
+++ РКИС/07.04/pr12/09.py
@@ -1,1 +1,13 @@
 
+# Задача 9 — Использование else
+def safe_print_number(s):
+    try:
+        num = int(s)
+    except ValueError:
+        print("Error")
+    else:
+        print(num)
+
+safe_print_number("5")
+safe_print_number("abc")
+
