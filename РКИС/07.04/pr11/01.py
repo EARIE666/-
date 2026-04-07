f634df2142ca01fdaa5046a9e016b33ab78f652f
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-04-07 14:16:47 +0500

    Update 01.py

--- РКИС/07.04/pr11/01.py
+++ РКИС/07.04/pr11/01.py
@@ -1,1 +1,19 @@
 
+# ================== Задача 1 — Переопределение метода ==================
+class Animal:
+    def speak(self):
+        print("Some sound")
+
+class Dog(Animal):
+    def speak(self):
+        print("Woof")
+
+class Cat(Animal):
+    def speak(self):
+        print("Meow")
+
+# Проверка задачи 1
+print("=== Задача 1 ===")
+animals = [Dog(), Cat(), Animal()]
+for a in animals:
+    a.speak()
