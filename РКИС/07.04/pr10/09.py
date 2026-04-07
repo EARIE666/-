c8bb84669d1ce398fb8e3ccc85460c71da19e9c0
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-04-07 14:25:14 +0500

    Update 09.py

--- РКИС/07.04/pr10/09.py
+++ РКИС/07.04/pr10/09.py
@@ -1,1 +1,20 @@
 
+# ================== Задача 9 — Переопределение инкремента ==================
+class Counter:
+    def __init__(self, value):
+        self.value = value
+    
+    def __iadd__(self, other):
+        self.value += other
+        return self
+    
+    def get_value(self):
+        return self.value
+
+# Проверка задачи 9
+print("\n=== Задача 9 ===")
+c = Counter(5)
+c += 3
+print(c.get_value())
+
+
