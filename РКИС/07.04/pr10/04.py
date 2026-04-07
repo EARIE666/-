c061e8a24a03de04d502194bf80b2abb1450ca6c
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-04-07 14:23:40 +0500

    Update 04.py

--- РКИС/07.04/pr10/04.py
+++ РКИС/07.04/pr10/04.py
@@ -1,1 +1,20 @@
 
+# ================== Задача 4 — Переопределение деления ==================
+class Divider:
+    def __init__(self, value):
+        self.value = value
+    
+    def __truediv__(self, other):
+        return Divider(self.value / other.value)
+    
+    def get_value(self):
+        return self.value
+
+# Проверка задачи 4
+print("\n=== Задача 4 ===")
+a = Divider(20)
+b = Divider(4)
+c = a / b
+print(c.get_value())
+
+
