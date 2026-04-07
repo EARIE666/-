6936957b091848d1b748bda0e6083701b334a467
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-04-07 14:17:10 +0500

    Update 02.py

--- РКИС/07.04/pr11/02.py
+++ РКИС/07.04/pr11/02.py
@@ -1,1 +1,13 @@
 
+# ================== Задача 2 — Перегрузка метода с разными аргументами ==================
+class Multiplier:
+    def multiply(self, a, b=None):
+        if b is None:
+            return a * a
+        return a * b
+
+# Проверка задачи 2
+print("\n=== Задача 2 ===")
+m = Multiplier()
+print(m.multiply(5))
+print(m.multiply(2, 3))
