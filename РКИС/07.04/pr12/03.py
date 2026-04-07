d2a1ba81e9a05b96a08dad8f158a0dbae7c0ac5e
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-04-07 14:27:38 +0500

    Update 03.py

--- РКИС/07.04/pr12/03.py
+++ РКИС/07.04/pr12/03.py
@@ -1,1 +1,15 @@
 
+# Задача 3 — Обработка нескольких исключений
+def safe_operation(a, b):
+    try:
+        return a / b
+    except ZeroDivisionError:
+        return "Division by zero"
+    except TypeError:
+        return "Type error"
+
+print(safe_operation(10, 2))
+print(safe_operation(5, 0))
+print(safe_operation(5, "a"))
+
+
