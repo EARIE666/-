28f4aa98a5d98e57fbb2e4b36a21e08455c2a4a5
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-04-07 14:24:42 +0500

    Update 07.py

--- РКИС/07.04/pr10/07.py
+++ РКИС/07.04/pr10/07.py
@@ -1,1 +1,15 @@
 
+# ================== Задача 7 — Переопределение строки ==================
+class Person:
+    def __init__(self, name):
+        self.name = name
+    
+    def __str__(self):
+        return f"Person: {self.name}"
+
+# Проверка задачи 7
+print("\n=== Задача 7 ===")
+p = Person("Alice")
+print(p)
+
+
