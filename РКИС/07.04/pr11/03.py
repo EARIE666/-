170751100df979599681811b5924baa09a7289f1
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-04-07 14:17:26 +0500

    Update 03.py

--- РКИС/07.04/pr11/03.py
+++ РКИС/07.04/pr11/03.py
@@ -1,1 +1,11 @@
 
+# ================== Задача 3 — Переопределение конструктора ==================
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+class Employee(Person):
+    def __init__(self, name, age, position):
+        super().__init__(name, age)
+        self.position = position
