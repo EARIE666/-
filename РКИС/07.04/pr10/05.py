250d9a6def4b2abf65a4e14503631a25a83ba543
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-04-07 14:23:58 +0500

    Update 05.py

--- РКИС/07.04/pr10/05.py
+++ РКИС/07.04/pr10/05.py
@@ -1,1 +1,20 @@
 
+# ================== Задача 5 — Переопределение сравнения ==================
+class ComparableNumber:
+    def __init__(self, value):
+        self.value = value
+    
+    def __lt__(self, other):
+        return self.value < other.value
+    
+    def get_value(self):
+        return self.value
+
+# Проверка задачи 5
+print("\n=== Задача 5 ===")
+a = ComparableNumber(5)
+b = ComparableNumber(10)
+print(a < b)
+print(b < a)
+
+
