43018089dc00b018d55d15921d73798d7da92910
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-04-08 12:08:29 +0500

    Update 08.py

--- РКИС/08.04/pr13/08.py
+++ РКИС/08.04/pr13/08.py
@@ -1,1 +1,16 @@
 
+# Задача 8
+class Counter:
+    count = 0
+    
+    def __init__(self):
+        Counter.count += 1
+    
+    @classmethod
+    def get_count(cls):
+        return cls.count
+
+c1 = Counter()
+c2 = Counter()
+print(Counter.get_count())  # 2
+
