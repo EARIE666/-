61c3123978b005d5b36ea53651fd26e54055b12f
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-04-08 12:07:46 +0500

    Update 06.py

--- РКИС/08.04/pr13/06.py
+++ РКИС/08.04/pr13/06.py
@@ -1,1 +1,10 @@
 
+# Задача 6
+class Validator:
+    @staticmethod
+    def is_positive(n):
+        return n > 0
+
+print(Validator.is_positive(5))   # True
+print(Validator.is_positive(-3))  # False
+
