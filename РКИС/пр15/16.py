00fb9fb5273dde1ac991cb8d24de06426cb771ee
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-04-15 15:10:10 +0500

    Update 16.py

--- РКИС/пр15/16.py
+++ РКИС/пр15/16.py
@@ -1,1 +1,8 @@
 # File: 16.py
+from datetime import datetime
+
+def task16():
+    now = datetime.now()
+    # или заданная: now = datetime(2024, 1, 1, 14, 30)
+    formatted = now.strftime("%d %B %Y, %H:%M")
+    print(formatted)
