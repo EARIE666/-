2616b400fbe2be633b7892627707846e1a3bbe0a
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-04-15 15:07:32 +0500

    Update 01.py

--- РКИС/пр15/01.py
+++ РКИС/пр15/01.py
@@ -1,1 +1,7 @@
 # File: 01.py
+def task1():
+    with open('input.txt', 'r', encoding='utf-8') as f:
+        text = f.read()
+    words = text.lower().split()
+    unique_words = set(words)
+    print(len(unique_words))
