8ebd280c3d76a1b67560107b4ddfea8e7ba1f5d8
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-04-15 15:10:27 +0500

    Update 18.py

--- РКИС/пр15/18.py
+++ РКИС/пр15/18.py
@@ -1,1 +1,10 @@
 # File: 18.py
+from datetime import datetime
+
+def task18():
+    deadline = datetime(2026, 3, 1)
+    now = datetime.now()
+    if now > deadline:
+        print("Дедлайн просрочен!")
+    else:
+        print("Еще есть время.")
