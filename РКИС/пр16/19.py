2a4ab534297dfb4ba2069511c026c34ffb74f177
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-04-22 15:03:09 +0500

    Update 19.py

--- РКИС/пр16/19.py
+++ РКИС/пр16/19.py
@@ -1,7 +1,13 @@
 # File: 19.py
-from datetime import datetime
+text = "Name: John Age: 25"
 
-def task19(message):
-    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    with open('log.txt', 'a', encoding='utf-8') as f:
-        f.write(f"[{timestamp}] {message}\n")
+pattern = r"Name:\s*(\w+)\s+Age:\s*(\d+)"
+
+match = re.search(pattern, text)
+
+if match:
+    name = match.group(1)
+    age = match.group(2)
+    
+    print(f"Имя: {name}")
+    print(f"Возраст: {age}")
