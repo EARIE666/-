0f28f84bf6e4df5c6dd6e11f7781152a9a171758
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-04-22 13:50:59 +0500

    Update 10.py

--- РКИС/пр16/10.py
+++ РКИС/пр16/10.py
@@ -1,9 +1,8 @@
 # File: 10.py
-def task10():
-    with open('input.txt', 'r', encoding='utf-8') as f:
-        lines = f.readlines()
-    
-    unique_lines = list(dict.fromkeys(lines)) # сохраняет порядок
-    
-    with open('unique.txt', 'w', encoding='utf-8') as f:
-        f.writelines(unique_lines)
+import re   
+text = "apple, banana orange,grape"
+
+def task10(text):
+    return re.findall(r"\b\w+", text)
+        
+print(task10(text))
