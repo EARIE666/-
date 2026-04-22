91607477f234944d01c312f638c18807dd55a335
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-04-22 14:45:06 +0500

    Update 16.py

--- РКИС/пр16/16.py
+++ РКИС/пр16/16.py
@@ -1,8 +1,12 @@
 # File: 16.py
-from datetime import datetime
+import re   
+text = "213.123.56.132"
 
-def task16():
-    now = datetime.now()
-    # или заданная: now = datetime(2024, 1, 1, 14, 30)
-    formatted = now.strftime("%d %B %Y, %H:%M")
-    print(formatted)
+def task16(text):
+    match = re.search(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", text)
+    if match:
+        return True
+    else:
+        return False
+        
+print(task16(text))
