fce8a09121215ac9c4ab0da63816a0a98fc93020
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-04-22 13:18:04 +0500

    Update 04.py

--- РКИС/пр16/04.py
+++ РКИС/пр16/04.py
@@ -1,12 +1,7 @@
 # File: 04.py
-def task4():
-    total_age = 0
-    count = 0
-    with open('data.csv', 'r', encoding='utf-8') as f:
-        next(f) # Пропускаем заголовок
-        for line in f:
-            name, age = line.strip().split(',')
-            total_age += int(age)
-            count += 1
-    if count > 0:
-        print(total_age / count)
+import re   
+text = "Hell1o world Python"
+
+def task4(text):
+    return re.sub(r"\s", "_", text)
+print(task4(text))
