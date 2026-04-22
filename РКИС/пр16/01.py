9ca7631206feccdf8b40b6733b74e8c4ee257b58
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-04-22 12:39:38 +0500

    Update 01.py

--- РКИС/пр16/01.py
+++ РКИС/пр16/01.py
@@ -1,7 +1,12 @@
 # File: 01.py
-def task1():
-    with open('input.txt', 'r', encoding='utf-8') as f:
-        text = f.read()
-    words = text.lower().split()
-    unique_words = set(words)
-    print(len(unique_words))
+import re   
+text = "I love Python programming"
+match = re.search("python", text, re.I)
+
+def task1(text):
+    if match:
+        return "Да"
+    else:
+        return "Нет"
+        
+print(task1(text))
