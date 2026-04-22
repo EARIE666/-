82be0570ea26570390cf5f0054161b01d3b181d5
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-04-22 12:48:32 +0500

    Update 02.py

--- РКИС/пр16/02.py
+++ РКИС/пр16/02.py
@@ -1,9 +1,9 @@
 # File: 02.py
-def task2():
-    from collections import Counter
-    with open('input.txt', 'r', encoding='utf-8') as f:
-        text = f.read()
-    words = text.lower().split()
-    counter = Counter(words)
-    for word, count in counter.most_common():
-        print(f"{word}: {count}")
+import re   
+text = "There are 12 apples and 5 bananas"
+
+def task2(text):
+    match = re.findall(r"\d+", text)
+    return match
+        
+print(task2(text))
