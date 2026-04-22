5b6662c86480a3422c39f7101667cfa8033a082f
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-04-22 13:58:57 +0500

    Update 11.py

--- РКИС/пр16/11.py
+++ РКИС/пр16/11.py
@@ -1,9 +1,8 @@
 # File: 11.py
-from datetime import datetime
+import re   
+text = "app, banana orange, gra"
 
-def task11():
-    date_str = "2024-12-31"
-    date_obj = datetime.strptime(date_str, "%Y-%m-%d")
-    print(f"День: {date_obj.day}")
-    print(f"Месяц: {date_obj.month}")
-    print(f"Год: {date_obj.year}")
+def task9(text):
+    return re.sub(r"\b\w{3}\b", "***", text)
+        
+print(task9(text))
