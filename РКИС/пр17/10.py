149b69ae27506d16dbfd5a6d282d6c6da09e9679
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-04-28 17:08:47 +0500

    Update 10.py

--- РКИС/пр17/10.py
+++ РКИС/пр17/10.py
@@ -1,9 +1,3 @@
-# File: 10.py
-def task10():
-    with open('input.txt', 'r', encoding='utf-8') as f:
-        lines = f.readlines()
-    
-    unique_lines = list(dict.fromkeys(lines)) # сохраняет порядок
-    
-    with open('unique.txt', 'w', encoding='utf-8') as f:
-        f.writelines(unique_lines)
+from datetime import datetime, timedelta
+
+print(datetime.today() + timedelta(days=7))
