91871a9acd2fa11da9a791f1744077f13ab93ddf
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-04-28 17:09:46 +0500

    Update 11.py

--- РКИС/пр17/11.py
+++ РКИС/пр17/11.py
@@ -1,9 +1,5 @@
-# File: 11.py
-from datetime import datetime
+import os
 
-def task11():
-    date_str = "2024-12-31"
-    date_obj = datetime.strptime(date_str, "%Y-%m-%d")
-    print(f"День: {date_obj.day}")
-    print(f"Месяц: {date_obj.month}")
-    print(f"Год: {date_obj.year}")
+# Получить список файлов и папок в текущей директории
+files = os.listdir('.')
+print(files)
