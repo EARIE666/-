9262865c7ea062051cde6fe7601deb09ef072515
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-04-28 16:38:13 +0500

    Update 03.py

--- РКИС/пр17/03.py
+++ РКИС/пр17/03.py
@@ -1,13 +1,3 @@
-# File: 03.py
-import string
-
-def task3():
-    with open('input.txt', 'r', encoding='utf-8') as f:
-        text = f.read()
-    
-    # Удаляем все знаки пунктуации
-    translator = str.maketrans('', '', string.punctuation)
-    clean_text = text.translate(translator)
-    
-    with open('clean.txt', 'w', encoding='utf-8') as f:
-        f.write(clean_text)
+import math
+x = 3.7
+print(math.floor(x), math.ceil(x))
