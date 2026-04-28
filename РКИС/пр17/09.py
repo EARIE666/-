a3313e65a96fd7b449c65ea3a8b133e9841f9ece
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-04-28 16:57:32 +0500

    Update 09.py

--- РКИС/пр17/09.py
+++ РКИС/пр17/09.py
@@ -1,8 +1,4 @@
-# File: 09.py
-def task9():
-    filenames = ['file1.txt', 'file2.txt']
-    with open('result.txt', 'w', encoding='utf-8') as outfile:
-        for fname in filenames:
-            with open(fname, 'r', encoding='utf-8') as infile:
-                outfile.write(infile.read())
-                outfile.write('\n') # разделитель между файлами
+import datetime
+date1 = datetime.date(2002, 12, 2)
+date2 = datetime.date(1991, 1, 2)
+print(date1-date2)
