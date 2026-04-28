264f05f1551b435fb623c1529eba6d108b1bb0c6
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-04-28 17:11:02 +0500

    Update 15.py

--- РКИС/пр17/15.py
+++ РКИС/пр17/15.py
@@ -1,9 +1,11 @@
-# File: 15.py
-from datetime import datetime
+import json
 
-def task15():
-    dates_str = ["2023-05-10", "2020-01-01", "2024-12-31"]
-    dates_obj = [datetime.strptime(d, "%Y-%m-%d") for d in dates_str]
-    dates_obj.sort()
-    for d in dates_obj:
-        print(d.date())
+data = {
+    "name": "Ivan",
+    "age": 25,
+    "skills": ["python", "data"]
+}
+
+with open('data.json', 'w', encoding='utf-8') as f:
+    json.dump(data, f, indent=4)
+print("Данные сохранены в data.json")
