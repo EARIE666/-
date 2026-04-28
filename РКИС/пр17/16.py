6ebe6547015ae89c7fb75836d1fb6abd764f833a
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-04-28 17:11:14 +0500

    Update 16.py

--- РКИС/пр17/16.py
+++ РКИС/пр17/16.py
@@ -1,8 +1,7 @@
-# File: 16.py
-from datetime import datetime
+import json
 
-def task16():
-    now = datetime.now()
-    # или заданная: now = datetime(2024, 1, 1, 14, 30)
-    formatted = now.strftime("%d %B %Y, %H:%M")
-    print(formatted)
+with open('data.json', 'r', encoding='utf-8') as f:
+    data = json.load(f)
+
+print(data)
+print(type(data))
