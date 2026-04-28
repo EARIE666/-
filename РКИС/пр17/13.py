4749af7f5b8b91e2060a4c47a6ca7ed4e4238111
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-04-28 17:10:15 +0500

    Update 13.py

--- РКИС/пр17/13.py
+++ РКИС/пр17/13.py
@@ -1,10 +1,5 @@
-# File: 13.py
-from datetime import datetime, timedelta
+import os
 
-def task13():
-    given_date = datetime(2026, 4, 9) # четверг
-    days_ahead = 7 - given_date.weekday() # 0 = Пн, значит если сегодня Пн, добавим 7
-    if days_ahead <= 0:
-        days_ahead += 7
-    next_monday = given_date + timedelta(days=days_ahead)
-    print(next_monday.date())
+if os.path.exists('data.txt'):
+    file_size = os.path.getsize('data.txt')
+    print(f"Размер файла: {file_size} байт")
