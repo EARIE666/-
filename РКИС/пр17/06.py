42a7d5180df3bc1dfcce6767360810e5095c6f2b
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-04-28 16:45:19 +0500

    Update 06.py

--- РКИС/пр17/06.py
+++ РКИС/пр17/06.py
@@ -1,12 +1,4 @@
-# File: 06.py
-def task6():
-    max_len = 0
-    max_line = ""
-    with open('input.txt', 'r', encoding='utf-8') as f:
-        for line in f:
-            line = line.rstrip('\n')
-            if len(line) > max_len:
-                max_len = len(line)
-                max_line = line
-    print(f"Длина: {max_len}")
-    print(f"Строка: {max_line}")
+import random
+list = [1, 2, 3, 4, 5]
+random.shuffle(list)
+print(list)
