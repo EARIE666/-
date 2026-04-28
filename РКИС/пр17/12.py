88d9c4851f532e1919c7d51a5c0355c4735876ad
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-04-28 17:10:00 +0500

    Update 12.py

--- РКИС/пр17/12.py
+++ РКИС/пр17/12.py
@@ -1,14 +1,4 @@
-# File: 12.py
-from datetime import datetime, timedelta
+import os
 
-def task12():
-    start = datetime(2026, 4, 1)
-    end = datetime(2026, 4, 15)
-    
-    current = start
-    workdays = 0
-    while current <= end:
-        if current.weekday() < 5: # Пн-Пт
-            workdays += 1
-        current += timedelta(days=1)
-    print(workdays)
+file_exists = os.path.exists('data.txt')
+print(f"Файл существует: {file_exists}")
