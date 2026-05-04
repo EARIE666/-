cbe7eee09ea8e349d276d13b04e36621ad9be4e1
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-05-04 15:05:39 +0500

    Update 08.py

--- РКИС/пр18/08.py
+++ РКИС/пр18/08.py
@@ -1,2 +1,6 @@
-import datetime
-print(datetime.date(2000, 12, 2))
+with open('input.txt', 'r', encoding='utf-8') as InpFile:
+    text = InpFile.read()
+    if not text:
+        print('Empty')
+    else:
+        print(text)
