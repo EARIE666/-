9b3b1946c4986c0286575b6f3be250be0c77dd5f
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-05-04 15:02:03 +0500

    Update 07.py

--- РКИС/пр18/07.py
+++ РКИС/пр18/07.py
@@ -1,2 +1,3 @@
-from datetime import datetime
-print(datetime.today())
+with open('input.txt', 'r', encoding='utf-8') as InpFile, open('output.txt', 'w', encoding='utf-8') as OutFile:
+    content = InpFile.read()
+    OutFile.write(content)
