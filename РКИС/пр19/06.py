afbc56f44200c79cd91bc1594f9404a3fbb7a53a
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-05-04 15:51:40 +0500

    Update 06.py

--- РКИС/пр19/06.py
+++ РКИС/пр19/06.py
@@ -1,4 +1,19 @@
-import random
-list = [1, 2, 3, 4, 5]
-random.shuffle(list)
-print(list)
+import threading
+
+counter = 0
+lock = threading.Lock()
+
+def safe_increment():
+    global counter
+    for _ in range(1000):
+        with lock:
+            counter += 1
+
+threads = [threading.Thread(target=safe_increment) for _ in range(10)]
+
+for t in threads:
+    t.start()
+for t in threads:
+    t.join()
+
+print(f"Финальное значение счётчика: {counter}")
