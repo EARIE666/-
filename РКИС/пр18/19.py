2db4e8f9e361da28abb7e4589a6dddade37fd387
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-05-04 15:42:53 +0500

    Update 19.py

--- РКИС/пр18/19.py
+++ РКИС/пр18/19.py
@@ -1,14 +1,13 @@
-import random
-from datetime import datetime, timedelta
+class SimpleContextManager:
 
-def get_random_date_2024():
-    start_date = datetime(2024, 1, 1)
-    end_date = datetime(2024, 12, 31)
-    
-    # Разница в днях
-    delta = end_date - start_date
-    random_days = random.randint(0, delta.days)
-    
-    return start_date + timedelta(days=random_days)
+    def __enter__(self):
+        print("Start")
+        return self
 
-print(get_random_date_2024().strftime('%Y-%m-%d'))
+    def __exit__(self, exc_type, exc_value, traceback):
+        print("End")
+        return False
+
+# Пример использования
+with SimpleContextManager():
+    print("Выполняется код внутри блока with")
