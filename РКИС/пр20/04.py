a38d8d5f2cb47bce1ff6974710b5f5ad0783a08e
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-05-04 16:00:27 +0500

    Update 04.py

--- РКИС/пр20/04.py
+++ РКИС/пр20/04.py
@@ -1,3 +1,19 @@
-import math
-x = 3.7
-print(math.floor(x), math.ceil(x))
+class PrivateStorageDescriptor:
+    def __init__(self, name):
+        self.name = name
+
+    def __get__(self, instance, owner):
+        return instance.__dict__.get(self.name)
+
+    def __set__(self, instance, value):
+        instance.__dict__[self.name] = value
+
+
+class MyClass:
+    attr = PrivateStorageDescriptor('_hidden_attr')
+
+
+obj = MyClass()
+obj.attr = 100
+print(obj.attr)
+print(obj.__dict__)
