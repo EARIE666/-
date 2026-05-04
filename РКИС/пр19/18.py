d8dff98e59e7250f47e51bfbd2a5b41111e2b4e8
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-05-04 15:55:09 +0500

    Update 18.py

--- РКИС/пр19/18.py
+++ РКИС/пр19/18.py
@@ -1,6 +1,17 @@
-import csv
+import asyncio
 
-with open('users.csv', 'r', encoding='utf-8') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        print(row)
+async def async_read_file(filename):
+    print(f"Начало чтения файла {filename}")
+    await asyncio.sleep(2)  # Имитация задержки чтения
+    content = f"Содержимое файла {filename}"
+    print(f"Файл {filename} прочитан")
+    return content
+
+async def main():
+    files = ["file1.txt", "file2.txt", "file3.txt"]
+    reads = [async_read_file(f) for f in files]
+    results = await asyncio.gather(*reads)
+    for content in results:
+        print(content)
+
+asyncio.run(main())
