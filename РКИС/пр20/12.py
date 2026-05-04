91f8aac654ae3f872dcc33cc1e61203624e1040a
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-05-04 16:02:04 +0500

    Update 12.py

--- РКИС/пр20/12.py
+++ РКИС/пр20/12.py
@@ -1,4 +1,22 @@
-import os
+class AgeDescriptor:
+    def __init__(self):
+        self._value = None
 
-file_exists = os.path.exists('data.txt')
-print(f"Файл существует: {file_exists}")
+    def __get__(self, instance, owner):
+        return self._value
+
+    def __set__(self, instance, value):
+        if not isinstance(value, int):
+            raise TypeError("Age must be int")
+        if value < 0 or value > 120:
+            raise ValueError("Age must be between 0 and 120")
+        self._value = value
+
+
+class MyClass:
+    age = AgeDescriptor()
+
+
+obj = MyClass()
+obj.age = 25
+# obj.age = 150  # ValueError
