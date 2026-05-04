7666fc8a90aadf34f701c102fd477f07cf244e43
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-05-04 16:01:19 +0500

    Update 08.py

--- РКИС/пр20/08.py
+++ РКИС/пр20/08.py
@@ -1,2 +1,22 @@
-import datetime
-print(datetime.date(2000, 12, 2))
+class DefaultValueDescriptor:
+    def __init__(self, default='default'):
+        self.default = default
+        self._value = None
+
+    def __get__(self, instance, owner):
+        if self._value is None:
+            return self.default
+        return self._value
+
+    def __set__(self, instance, value):
+        self._value = value
+
+
+class MyClass:
+    attr = DefaultValueDescriptor()
+
+
+obj = MyClass()
+print(obj.attr)  # default
+obj.attr = "test"
+print(obj.attr)  # test
