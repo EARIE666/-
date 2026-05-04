c27017ec5e3a509c974947585a79fcc4a8d2af03
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-05-04 15:38:52 +0500

    Update 14.py

--- РКИС/пр18/14.py
+++ РКИС/пр18/14.py
@@ -1,6 +1,4 @@
-import os
-
-folder = 'folder'
-file_name = 'file.txt'
-full_path = os.path.join(folder, file_name)
-print(full_path) 
+with open('input.txt', 'r', encoding='utf-8') as InpFile, open('output.txt', 'a', encoding='utf-8') as OutFile:
+    for line in InpFile:
+        if line.strip():  # фильтрует строки с пробелами/\n с обеих сторон
+            OutFile.write(line)
