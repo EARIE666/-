d9a6c0595d9153edab232ce5b0a6c2bf9094f163
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-05-04 16:00:56 +0500

    Update 06.py

--- РКИС/пр20/06.py
+++ РКИС/пр20/06.py
@@ -1,4 +1,20 @@
-import random
-list = [1, 2, 3, 4, 5]
-random.shuffle(list)
-print(list)
+class PositiveNumberDescriptor:
+    def __init__(self):
+        self._value = None
+
+    def __get__(self, instance, owner):
+        return self._value
+
+    def __set__(self, instance, value):
+        if value <= 0:
+            raise ValueError("Value must be positive")
+        self._value = value
+
+
+class MyClass:
+    attr = PositiveNumberDescriptor()
+
+
+obj = MyClass()
+obj.attr = 42
+# obj.attr = -5  # ValueError
