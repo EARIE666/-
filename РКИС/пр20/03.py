327480104cf5d0e1bab7804bc1344a0d8178e0dd
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-05-04 16:00:14 +0500

    Update 03.py

--- РКИС/пр20/03.py
+++ РКИС/пр20/03.py
@@ -1,3 +1,18 @@
-import math
-x = 3.7
-print(math.floor(x), math.ceil(x))
+class SetLoggingDescriptor:
+    def __init__(self):
+        self._value = None
+
+    def __get__(self, instance, owner):
+        return self._value
+
+    def __set__(self, instance, value):
+        print('Setting value')
+        self._value = value
+
+
+class MyClass:
+    attr = SetLoggingDescriptor()
+
+
+obj = MyClass()
+obj.attr = 5
