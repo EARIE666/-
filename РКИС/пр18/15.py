85f17a01e361c4cb312c1e2b405274937bc44658
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-05-04 15:40:31 +0500

    Update 15.py

--- РКИС/пр18/15.py
+++ РКИС/пр18/15.py
@@ -1,11 +1,7 @@
-import json
 
-data = {
-    "name": "Ivan",
-    "age": 25,
-    "skills": ["python", "data"]
-}
+import csv
 
-with open('data.json', 'w', encoding='utf-8') as f:
-    json.dump(data, f, indent=4)
-print("Данные сохранены в data.json")
+with open('data.csv', 'r', encoding='utf-8') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        print(row)
