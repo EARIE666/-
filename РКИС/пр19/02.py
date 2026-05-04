f85474bc2c306fbebf8b46dcaf7789d8e76d0e0f
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-05-04 15:50:48 +0500

    Update 02.py

--- РКИС/пр19/02.py
+++ РКИС/пр19/02.py
@@ -1,2 +1,16 @@
-import math
-print(math.factorial(5))
+import threading
+import time
+
+def print_numbers(name):
+    for i in range(1, 6):
+        print(f"{name}: {i}")
+        time.sleep(0.5)
+
+t1 = threading.Thread(target=print_numbers, args=("Thread-1",))
+t2 = threading.Thread(target=print_numbers, args=("Thread-2",))
+
+t1.start()
+t2.start()
+
+t1.join()
+t2.join()
