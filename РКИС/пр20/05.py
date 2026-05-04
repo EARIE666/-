7bc732e5faccf9fd68517d32ad7e3be3d09e8d11
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-05-04 16:00:37 +0500

    Update 05.py

--- РКИС/пр20/05.py
+++ РКИС/пр20/05.py
@@ -1,2 +1,20 @@
-import random
-print(random.randint(1, 10))
+class IntOnlyDescriptor:
+    def __init__(self):
+        self._value = None
+
+    def __get__(self, instance, owner):
+        return self._value
+
+    def __set__(self, instance, value):
+        if not isinstance(value, int):
+            raise TypeError("Value must be int")
+        self._value = value
+
+
+class MyClass:
+    attr = IntOnlyDescriptor()
+
+
+obj = MyClass()
+obj.attr = 10
+# obj.attr = "hello"  # TypeError
