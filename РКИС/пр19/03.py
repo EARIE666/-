69faf636f143aefddba04b998a31818ebc78e6fc
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-05-04 15:51:03 +0500

    Update 03.py

--- РКИС/пр19/03.py
+++ РКИС/пр19/03.py
@@ -1,3 +1,16 @@
-import math
-x = 3.7
-print(math.floor(x), math.ceil(x))
+import threading
+import time
+
+def print_name(name):
+    for _ in range(5):
+        print(f"Поток {name}")
+        time.sleep(0.3)
+
+threads = []
+for i in range(3):
+    t = threading.Thread(target=print_name, args=(f"Thread-{i+1}",))
+    threads.append(t)
+    t.start()
+
+for t in threads:
+    t.join()
