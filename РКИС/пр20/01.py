b0e8a56336101942d1256bea41e7fb987390bcb0
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-05-04 15:57:05 +0500

    Update 01.py

--- РКИС/пр20/01.py
+++ РКИС/пр20/01.py
@@ -1,2 +1,8 @@
-import math
-print(math.sqrt(25))
+import asyncio
+
+async def simple_async_function():
+    print("Начало выполнения")
+    await asyncio.sleep(1)
+    print("Сообщение после задержки")
+
+asyncio.run(simple_async_function())
