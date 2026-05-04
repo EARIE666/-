afdc5b18c40016d99049fcd077bb88d837850070
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-05-04 16:01:08 +0500

    Update 07.py

--- РКИС/пр20/07.py
+++ РКИС/пр20/07.py
@@ -1,2 +1,20 @@
-from datetime import datetime
-print(datetime.today())
+class StringOnlyDescriptor:
+    def __init__(self):
+        self._value = None
+
+    def __get__(self, instance, owner):
+        return self._value
+
+    def __set__(self, instance, value):
+        if not isinstance(value, str):
+            raise TypeError("Value must be string")
+        self._value = value
+
+
+class MyClass:
+    attr = StringOnlyDescriptor()
+
+
+obj = MyClass()
+obj.attr = "hello"
+# obj.attr = 123  # TypeError
