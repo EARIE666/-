66ae9d8bc55b15b74450d577fad121b84d8323c6
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-05-04 15:59:54 +0500

    Update 02.py

--- РКИС/пр20/02.py
+++ РКИС/пр20/02.py
@@ -1,2 +1,19 @@
-import math
-print(math.factorial(5))
+class LoggingDescriptor:
+    def __init__(self):
+        self._value = None
+
+    def __get__(self, instance, owner):
+        print('Getting value')
+        return self._value
+
+    def __set__(self, instance, value):
+        self._value = value
+
+
+class MyClass:
+    attr = LoggingDescriptor()
+
+
+obj = MyClass()
+obj.attr = 10
+print(obj.attr)
