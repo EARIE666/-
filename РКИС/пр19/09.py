446e62f70460a02535041960a30c061ed5f1a35b
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-05-04 15:52:16 +0500

    Update 09.py

--- РКИС/пр19/09.py
+++ РКИС/пр19/09.py
@@ -1,4 +1,16 @@
-import datetime
-date1 = datetime.date(2002, 12, 2)
-date2 = datetime.date(1991, 1, 2)
-print(date1-date2)
+from multiprocessing import Process
+import time
+
+def calculate_sum(start, end, name):
+    total = sum(range(start, end + 1))
+    print(f"Процесс {name}: сумма от {start} до {end} = {total}")
+
+if __name__ == '__main__':
+    p1 = Process(target=calculate_sum, args=(1, 50000, "A"))
+    p2 = Process(target=calculate_sum, args=(50001, 100000, "B"))
+
+    p1.start()
+    p2.start()
+
+    p1.join()
+    p2.join()
