3517bb3f15a70b0d7c202d9aea6bca74ecbbe471
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-05-04 15:19:28 +0500

    Update 11.py

--- РКИС/пр18/11.py
+++ РКИС/пр18/11.py
@@ -1,5 +1,3 @@
-import os
-
-# Получить список файлов и папок в текущей директории
-files = os.listdir('.')
-print(files)
+with open('input.txt', 'r', encoding='utf-8') as InpFile, open('output.txt', 'w', encoding='utf-8') as OutFile:
+    content = InpFile.read()
+    OutFile.write(content)
