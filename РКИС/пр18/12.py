8390ee25a271bae98681b4af696ff9c8b84e3405
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-05-04 15:29:44 +0500

    Update 12.py

--- РКИС/пр18/12.py
+++ РКИС/пр18/12.py
@@ -1,4 +1,6 @@
-import os
-
-file_exists = os.path.exists('data.txt')
-print(f"Файл существует: {file_exists}")
+writeList = []
+with open('input.txt', 'r', encoding='utf-8') as InpFile, open('output.txt', 'w', encoding='utf-8') as OutFile:
+    for line in InpFile:
+        if len(line.rstrip()) > 5:
+            writeList.append(line.rstrip()) 
+    OutFile.write('\n'.join(writeList))
