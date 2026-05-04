58ef8ad85607bc8d63460e05aa698e3d8d68d289
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-05-04 16:01:29 +0500

    Update 09.py

--- РКИС/пр20/09.py
+++ РКИС/пр20/09.py
@@ -1,4 +1,26 @@
-import datetime
-date1 = datetime.date(2002, 12, 2)
-date2 = datetime.date(1991, 1, 2)
-print(date1-date2)
+class CounterDescriptor:
+    def __init__(self):
+        self._value = None
+        self._count = 0
+
+    def __get__(self, instance, owner):
+        self._count += 1
+        return self._value
+
+    def __set__(self, instance, value):
+        self._value = value
+
+    def get_count(self):
+        return self._count
+
+
+class MyClass:
+    attr = CounterDescriptor()
+
+
+obj = MyClass()
+obj.attr = 99
+print(obj.attr)
+print(obj.attr)
+print(obj.attr)
+print(MyClass.attr.get_count())  # 3
