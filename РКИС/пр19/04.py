a134b549471c33499c62b90a4157d1c00f82be6c
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-05-04 15:51:14 +0500

    Update 04.py

--- РКИС/пр19/04.py
+++ РКИС/пр19/04.py
@@ -1,3 +1,16 @@
-import math
-x = 3.7
-print(math.floor(x), math.ceil(x))
+import threading
+import time
+
+def delayed_task(name, delay):
+    print(f"{name} начал выполнение")
+    time.sleep(delay)
+    print(f"{name} завершил выполнение")
+
+t1 = threading.Thread(target=delayed_task, args=("Быстрый поток", 0.5))
+t2 = threading.Thread(target=delayed_task, args=("Медленный поток", 2))
+
+t1.start()
+t2.start()
+
+t1.join()
+t2.join()
