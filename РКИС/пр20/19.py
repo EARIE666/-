77e09382798f7bcac3487a86a8da519b34839c1e
TWERD <86604031+EARIE666@users.noreply.github.com>
2026-05-04 16:03:31 +0500

    Update 19.py

--- РКИС/пр20/19.py
+++ РКИС/пр20/19.py
@@ -1,14 +1,11 @@
-import random
-from datetime import datetime, timedelta
+class InstanceCounter:
+    count = 0
 
-def get_random_date_2024():
-    start_date = datetime(2024, 1, 1)
-    end_date = datetime(2024, 12, 31)
-    
-    # Разница в днях
-    delta = end_date - start_date
-    random_days = random.randint(0, delta.days)
-    
-    return start_date + timedelta(days=random_days)
+    def __init__(self):
+        InstanceCounter.count += 1
 
-print(get_random_date_2024().strftime('%Y-%m-%d'))
+
+a = InstanceCounter()
+b = InstanceCounter()
+c = InstanceCounter()
+print(InstanceCounter.count)  # 3
